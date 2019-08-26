df6d17cd6f7504119f12823cdfc4d1e0faa6192d
Anthony Hoareau <ah@moonda.com>
2019-08-26 14:30:10 +0200

    Feat: Add all books component

---
+++ src/app/book-list/all-books/all-books.component.ts
@@ -0,0 +1,55 @@
+import { UserModel } from 'src/app/models/user';
+import { Component, OnInit } from '@angular/core';
+import { BookModel } from 'src/app/models/book';
+import { Subscription } from 'rxjs';
+import { BooksService } from 'src/app/services/books.service';
+import { Router } from '@angular/router';
+import * as firebase from 'firebase';
+
+@Component({
+  selector: 'app-all-books',
+  templateUrl: './all-books.component.html',
+  styleUrls: ['./all-books.component.scss']
+})
+export class AllBooksComponent implements OnInit {
+
+  books: BookModel[];
+  bookSubscribe: Subscription;
+  owner = [];
+  borrow: boolean;
+
+  constructor(private bookService: BooksService, private router: Router) { }
+
+  ngOnInit() {
+    firebase.auth().onAuthStateChanged(
+      (user) => {
+        if (user) {
+          firebase.database().ref(user.uid).once('value', (data) => {
+            this.owner = data.val() ? data.val() : [];
+          });
+        }
+      }
+    );
+    this.bookSubscribe = this.bookService.allBookSubject.subscribe(
+      (book: BookModel[]) => {
+        this.books = book;
+      }
+    );
+    this.bookService.getAllBooks();
+    this.bookService.emitAllBooks();
+  }
+
+  onView(id: number, key: string) {
+    this.bookService.comeFrom(key);
+    this.router.navigate(['/books', 'view', id]);
+  }
+
+  isBorrow(book: BookModel) {
+    console.log(this.books);
+    this.borrow = true;
+    this.bookService.borrow(book);
+    console.log(this.borrow);
+    console.log(this.books);
+  }
+
+}
